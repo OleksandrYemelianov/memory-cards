15d3bee49d78d02e40cb531e787eea264190b70a
OleksandrYemelianov <yemcoder@gmail.com>
2026-05-24 21:01:46 +0200

    DI improve . v1.4

--- memory_cards/app/Services/DeeplTranslateService.php
+++ memory_cards/app/Services/DeeplTranslateService.php
@@ -11,7 +11,7 @@ class DeeplTranslateService implements TranslatorInterface
 
     public function __construct(private string $apiKey)
     {
-        if (empty($this->auth_key)) {
+        if (empty($this->apiKey)) {
             throw new \InvalidArgumentException("The API key is not set.");
         }
         $this->translator = new Translator($this->apiKey);
